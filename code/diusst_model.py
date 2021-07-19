28b4e449c1904f236b86f23fd59a8db7b37b8994
reykboerner <reyk.boerner@fu-berlin.de>
2021-07-19 11:01:49 +0200

    diusst modification: bottom boundary condition changed from constant ftemp (avg. of data) to varying ftemp from data timeseries

--- code/diusst_model.py
+++ code/diusst_model.py
@@ -20,6 +20,7 @@ from diusst_funcs import make_mesh, snell, s_sat, laplace_central, grad_central,
 def diusst_bayesian(
     times,                          # array of time points (in s)
     Ta_data,                        # air temperature data array
+    Tf_data,			    # foundation temperature data
     sw_data,                        # incoming shortwave radiation data array
     u_data=None,                    # (optional) wind speed data array (in m/s)
     sa_data=None,                   # (optional) air specific humidity data array (in kg/kg)
@@ -69,10 +70,10 @@ def diusst_bayesian(
     T = np.zeros((N_t, N_z))
 
     # initial condition
-    T[0] = np.ones(N_z)*T_f
+    T[0] = np.ones(N_z)*Tf_data[0]
 
     # boundary condition
-    T[:,-1] = np.ones(N_t)*T_f
+    T[:,-1] = Tf_data
 
     # Eddy diffusivity
     kappa = np.zeros((N_t,N_z))
