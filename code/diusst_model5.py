5627835d3cc26c8409f8f8bbe4c455be9e2e81e3
reykboerner <reyk.boerner@fu-berlin.de>
2021-07-26 03:26:47 +0200

    implemented 5-parameter version of DIUSST

---
+++ code/diusst_model5.py
@@ -0,0 +1,124 @@
+"""
+Diurnal SST model for Bayesian analysis
+Code boiled down to necessities in order to minimize computiation time
+cloud = 0
+wind_dep = quadratic
+diffu_type = 'linear'
+skin = False
+diffu = 1
+sens = 1
+latent = 1
+rad = 1
+surflux = 1
+"""
+
+# import required packages
+import numpy as np
+from diusst_funcs import make_mesh, snell, s_sat, laplace_central, grad_central, grad_backward, dndz, dndz2
+
+def diusst_bayesian(
+    times,                          # array of time points (in s)
+    Ta_data,                        # air temperature data array
+    sw_data,                        # incoming shortwave radiation data array
+    u_data=None,                    # (optional) wind speed data array (in m/s)
+    sa_data=None,                   # (optional) air specific humidity data array (in kg/kg)
+    T_f=300,                        # foundation temperature (in K)
+    z_f=10,                         # depth of modeled water column (in m)
+    dz=0.05,                        # depth resolution (if streched grid, this is the thickness of the top layer) (in m)
+    attenu=1,                       # light attenuation coefficient (in 1/m)
+    mu=1,                           # linear mixing coefficient μ (in 1/s)
+    k_eddy = 1e-4,                  # eddy diffusivity coefficient (in m^2/s)
+    k_0 = 1,                        # kappa 0 (4th parameter)
+    lambd = 1,                      # trapping depth lambda (in m) (5th parameter)
+    # constants
+    k_mol = 1e-7,                   # molecular diffusivity of sea water (in m^2/s)
+    n_w = 1.34,                     # refractive index of sea water
+    n_a = 1.0,                      # refractive index of air
+    rho_w = 1027,                   # density of sea water (in kg/m^3)
+    rho_a = 1.1,                    # density of air (in kg/m^3)
+    C_s = 1.3e-3,                   # turbulent exchange coefficient for sensible heat
+    C_l = 1.5e-3,                   # turbulent exchange coefficient for latent heat
+    L_evap = 2.5e6,                 # latent heat of evaporization (J/kg) Source: Klose p.151
+    c_p = 3850,                     # specific heat of sea water at const pressure (in J/K/kg)
+    c_p_a = 1005,                   # specific heat of air at const pressure (in J/K/kg)
+    sb_const = 5.67e-8,             # Stefan Boltzmann constant (in W/m^2/K^4)
+    R_v = 461.51,                   # gas constant of water vapor, J/K/kg):
+    # optional
+    t_sim=None,                     # duration of simulation (in s)
+    dt=None,                        # time step (in s)
+    init = None,                    # initial sea temperature of domain. if None, then T_f is used as the domain temperature (in K)
+    wind=0,                         # constant wind speed (if u_data is None) (in m/s)
+    humid = 10*1e-3,                # constant specific humidity at reference height above sea level (if sa_data is None) (in kg/kg)
+    # switches
+    maxwind = 10,
+    ngrid = None,                   # number of vertical grid points (excluding foundation and air dummy). If None, then a uniform mesh is generated with grid spacing dz
+    opac=1,):                       # opacity of the vertically integrated atmosphere to longwave radiation
+
+    # Define depth mesh
+    N_z = ngrid + 2
+    z, stretch = make_mesh(dz,ngrid,z_f=z_f)
+
+    # Define time array
+    N_t = len(times)
+    dt = times[1:]-times[:-1]
+
+    # Atmospheric input data
+    s_a = sa_data
+    u = u_data
+
+    # Initialize temperature array and heat flux arrays
+    T = np.zeros((N_t, N_z))
+
+    # initial condition
+    T[0] = np.ones(N_z)*T_f
+
+    # boundary condition
+    T[:,-1] = np.ones(N_t)*T_f
+
+    # Eddy diffusivity
+    kappa = np.zeros((N_t,N_z))
+    dkappadz = np.zeros((N_t,N_z))
+
+    for i in range(N_z):
+        #kappa[:,i] = (k_mol - k_eddy * z[i]/z_f *(np.minimum(u,np.ones(N_t)*maxwind))**2)
+        kappa[:,i] = k_mol + k_eddy * (1- k_0*np.exp(z[i]/lambd)) * (np.minimum(u,np.ones(N_t)*maxwind))**2
+        dkappadz[:,i] = - k_eddy*k_0/lambd * np.exp(z[i]/lambd) * (np.minimum(u,np.ones(N_t)*maxwind))**2
+
+    # mixing coefficient
+    mix = np.zeros(N_z)
+    mix[1:-1] = mu * np.abs(z[2:]-z[1:-1]) / np.abs(z[1:-1]-z[-1])
+
+    # initialize heat fluxes
+    Qs, Ql, Rlw = [], [], []
+    R_sw = np.zeros((N_t,N_z))
+
+    # solar irradiance
+    solangle = 2*np.pi*times/86400 + np.pi
+    for i in range(N_z):
+        R_sw[:,i] = sw_data * np.exp(attenu/np.cos(snell(solangle))*z[i])
+
+    # stretched grid prerequisites
+    dv1 = dndz(z, dz0=dz,eps=stretch)
+    dv2 = dndz2(z, dz0=dz,eps=stretch)
+
+    # Time integration
+    for n in range(1, N_t):
+
+        # surface fluxes
+        R_lw = sb_const * (opac*(Ta_data[n-1])**4 - (T[n-1,0])**4)
+        Q_s  = rho_a * c_p_a * C_s * max(0.5, u[n-1]) * (Ta_data[n-1] - T[n-1,0])
+        Q_l  = rho_a * L_evap * C_l * max(0.5, u[n-1]) * (s_a[n-1] - s_sat(T[n-1,0], rho_a, R_v))
+
+        # Total heat flux
+        Q = R_sw[n-1]
+        #Q[0] = rad*R_sw[n-1,1] + surflux*(R_lw + Q_s + Q_l)
+        Q[0] += (R_lw + Q_s + Q_l)
+
+        # Euler integration
+        T[n] = T[n-1] + dt[n-1] * (
+                kappa[n-1]*laplace_central(T[n-1],dv1,dv2) + dkappadz[n-1]*grad_central(T[n-1],dv1)
+                - mix*(T[n-1]-T_f)
+                + grad_backward(Q,dv1)/(rho_w*c_p)
+                )
+
+    return [T[:,1:], z[1:], times]
---
+++ code/run_bayesian5.py
@@ -0,0 +1,161 @@
+"""
+run_bayesian.py
+Python script to run Bayesian analysis for DIUSST model
+"""
+
+# Change working directory to curr
+import os
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# Load external modules
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import emcee
+import corner
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+# Load custom functions
+from interpolation import cfl_interpolation
+from diusst_model import diusst_bayesian as diusst
+
+# Time stamp
+from datetime import datetime
+now = datetime.now()
+timestamp = now.strftime("%y%m%d-%H%M%S")
+
+###########################################################
+# RUN SETTINGS (check before each run)
+
+# Output storage
+run_id = 'M-I4-B1'
+output_path = '../output/'
+
+# Fit parameters
+param_names = ['kappa', 'mu', 'attenu', 'kappa0', 'lambd']
+
+# Parameter limits
+param_min = np.array([0, 0, 1e-3, 0, 0.5])
+param_max = np.array([7e-4, 0.1, 10, 1, 20])
+
+# Initial walker positions
+param_start = np.array([2e-4, 1e-4, 2.5, 1, 1])
+
+# Sampling
+nwalkers = 32
+nsteps = 2000
+
+# DIUSST model
+scheme = 'euler'
+z_f = 10
+dz0 = 0.10
+ngrid = 50
+diffu = 1
+opac = 1
+k_mol = 1e-7
+maxwind = 10
+
+# Dataset
+data_path = '../data/bayesian_training/'
+data_filename = 'training_minnett_ssterr03-10_humid10.csv'
+data_interval = [1175,1377]
+
+# Other settings
+parallel = True
+use_backend = True
+
+###########################################################
+# (End of run settings)
+###########################################################
+
+print('==== Bayesian sampling 5 parameters, Run ID '+run_id+' ====')
+print('Start time: '+str(timestamp))
+print('Data loaded from '+data_path+data_filename)
+
+# Load dataset
+data_orig = pd.read_csv(data_path+data_filename)[data_interval[0]:data_interval[1]]
+
+# interpolate to meet CFL condition
+data = cfl_interpolation5(data_orig, dz0=dz0, ngrid=ngrid,
+        k_mol = k_mol,
+        k_eddy_max=param_max[0], k_0_max=param_max[3], lambd_min=param_min[-1]
+        maxwind=maxwind, z_f=z_f,
+        save=output_path+timestamp+'_'+run_id)[0]
+
+# extract data
+ftemp = np.mean(data['ftemp'].to_numpy(np.float64))
+sst_data = data['sst'].to_numpy(np.float64) - data['ftemp'].to_numpy(np.float64)
+sst_err = data['sst_err'].to_numpy(np.float64) * 0.01
+times = data['times'].to_numpy(np.float64)
+wind = data['wind'].to_numpy(np.float64)
+atemp = data['atemp'].to_numpy(np.float64)
+swrad = data['swrad'].to_numpy(np.float64)
+humid = data['humid'].to_numpy(np.float64)
+
+# Define likelihood function
+def bayesian_likelihood(params):
+    kappa, mu, attenu, kappa0, lambd = params
+    simu = diusst(
+            times, atemp, swrad, u_data=wind, sa_data=humid, T_f=ftemp,
+            k_eddy=kappa, mu=mu, attenu=attenu, k_0=kappa0, lambd=lambd,
+            opac=opac, k_mol=k_mol,
+            dz=dz0, ngrid=ngrid)
+    sst_model = simu[0][:,0] - ftemp
+    mse = np.mean(((sst_data-sst_model)/sst_err)**2)
+    return mse
+
+# Define posterior distribution function
+def log_prob(x):
+    if (x<param_min).any() or (x>param_max).any():
+        return -np.inf
+    else:
+        mse = bayesian_likelihood(x)
+        return - (mse + np.log( np.prod(param_max-param_min) ))
+        #return -mse
+
+# initialize emcee
+ndim = len(param_names)
+initial = np.zeros((nwalkers, ndim))
+for i in range(nwalkers):
+    initial[i] = param_start + 0.1*param_start*np.random.uniform(size=ndim)
+
+# prerequisites for parallelization
+os.environ["OMP_NUM_THREADS"] = "1"
+ncpu = cpu_count()
+
+print('Ndim = {0} parameters, sample = {1} steps.'.format(ndim, nsteps))
+print("{} walkers on {} CPUs.".format(nwalkers, ncpu))
+
+# Backend
+backend_filename = timestamp+'_'+run_id+'.h5'
+backend = emcee.backends.HDFBackend(backend_filename)
+backend.reset(nwalkers, ndim)
+
+# run MCMC sampler
+if parallel:
+    with Pool() as pool:
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool, backend=backend)
+        mcrun = sampler.run_mcmc(initial, nsteps, progress=True)
+
+else:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
+    mcrun = sampler.run_mcmc(initial, nsteps, progress=True)
+
+print('Sampling complete ({} steps).'.format(nsteps))
+
+# Get sampling results
+samples = sampler.get_chain()
+autocorrtime = sampler.get_autocorr_time(quiet=True)
+probs = sampler.get_log_prob()
+
+for i in range(ndim):
+	np.savetxt(output_path+timestamp+'_'+run_id+'_samples_'+param_names[i]+'.txt', samples[:,:,i])
+
+np.savetxt(output_path+timestamp+'_'+run_id+'_autocorrtime.txt', autocorrtime)
+np.savetxt(output_path+timestamp+'_'+run_id+'_probs.txt', probs)
+
+print('Sample storage complete.')
+print('=================================== Success!')
