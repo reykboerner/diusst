b3b5fa9b6cab0713de93cda4dd497d7cc911de53
reykboerner <reyk.boerner@fu-berlin.de>
2021-10-13 20:57:04 +0100

    New model script diusst_model.py containing BASE, LIN, EXP, and STAB. Renamed the old file diusst_model.py -> diusst_model_old.py

--- code/diusst_model.py
+++ code/diusst_model.py
@@ -1,119 +1,145 @@
 """
-Diurnal SST model for Bayesian analysis
-Code boiled down to necessities in order to minimize computiation time
-cloud = 0
-wind_dep = quadratic
-diffu_type = 'linear'
-skin = False
-diffu = 1
-sens = 1
-latent = 1
-rad = 1
-surflux = 1
+DiuSST model
+Featuring the BASE, LIN, EXP, and STAB versions
+Documentation:
+Author: Reyk
+Date: 13 Oct 2021
 """
 
 # import required packages
 import numpy as np
 from diusst_funcs import make_mesh, snell, s_sat, laplace_central, grad_central, grad_backward, dndz, dndz2
 
-def diusst_bayesian(
-    times,                          # array of time points (in s)
-    Ta_data,                        # air temperature data array
-    sw_data,                        # incoming shortwave radiation data array
-    u_data=None,                    # (optional) wind speed data array (in m/s)
-    sa_data=None,                   # (optional) air specific humidity data array (in kg/kg)
-    T_f=300,                        # foundation temperature (in K)
-    z_f=10,                         # depth of modeled water column (in m)
-    dz=0.05,                        # depth resolution (if streched grid, this is the thickness of the top layer) (in m)
-    attenu=1,                       # light attenuation coefficient (in 1/m)
-    mu=1,                           # linear mixing coefficient μ (in 1/s)
-    k_eddy = 1e-4,                  # eddy diffusivity coefficient (in m^2/s)
-    # constants
-    k_mol = 1e-7,                   # molecular diffusivity of sea water (in m^2/s)
-    n_w = 1.34,                     # refractive index of sea water
-    n_a = 1.0,                      # refractive index of air
-    rho_w = 1027,                   # density of sea water (in kg/m^3)
-    rho_a = 1.1,                    # density of air (in kg/m^3)
-    C_s = 1.3e-3,                   # turbulent exchange coefficient for sensible heat
-    C_l = 1.5e-3,                   # turbulent exchange coefficient for latent heat
-    L_evap = 2.5e6,                 # latent heat of evaporization (J/kg) Source: Klose p.151
-    c_p = 3850,                     # specific heat of sea water at const pressure (in J/K/kg)
-    c_p_a = 1005,                   # specific heat of air at const pressure (in J/K/kg)
-    sb_const = 5.67e-8,             # Stefan Boltzmann constant (in W/m^2/K^4)
-    R_v = 461.51,                   # gas constant of water vapor, J/K/kg):
-    # optional
-    t_sim=None,                     # duration of simulation (in s)
-    dt=None,                        # time step (in s)
-    init = None,                    # initial sea temperature of domain. if None, then T_f is used as the domain temperature (in K)
-    wind=0,                         # constant wind speed (if u_data is None) (in m/s)
-    humid = 10*1e-3,                # constant specific humidity at reference height above sea level (if sa_data is None) (in kg/kg)
-    # switches
-    maxwind = 10,
-    ngrid = None,                   # number of vertical grid points (excluding foundation and air dummy). If None, then a uniform mesh is generated with grid spacing dz
-    opac=1,):                       # opacity of the vertically integrated atmosphere to longwave radiation
+def diusst(
+    # Atmospheric input data
+    time_data,                  # array of time points (in s)
+    wind_data,                  # wind speed data array (in m/s)
+    swave_data,                 # incoming shortwave radiation data array (in W/m2)
+    airtemp_data,               # air temperature data array (in K)
+    humid_data,                 # air specific humidity data array (in kg/kg)
+    # Model parameters
+    T_f=300,                    # foundation temperature (in K)
+    kappa = 1e-4,               # eddy diffusivity coefficient (in m^2/s)
+    mu=1,                       # linear mixing coefficient μ (in 1/s)
+    alpha=1,                    # light attenuation coefficient (in 1/m)
+    lambd=3,                    # trapping depth (in m)
+    sigma=0.8,                  # surface suppressivity
+    z_ref=1,                    # reference depth for STAB model
+    # Model options
+    diffu_type='BASE',          # Type of vertical diffusivity profile
+    init = None,                # initial sea temperature of domain. if None, then T_f is used as the domain temperature (in K)
+    output = 'basic',           # What information the function outputs
+    wind_max = 10,              # Cut-off wind speed
+    wind_exp = 2,               # exponent of the wind speed in turbulent diffusivity
+    opac=1,                     # opacity of the vertically integrated atmosphere to longwave radiation
+    # Model domain
+    z_f=10,                     # Foundation depth (in m)
+    dz=0.05,                    # depth resolution (if streched grid, this is the thickness of the top layer) (in m)
+    ngrid = None,               # number of vertical grid points (excluding foundation and air dummy). If None, then a uniform mesh is generated with grid spacing dz
+    # Constants
+    k_mol = 1e-7,               # molecular diffusivity of sea water (in m^2/s)
+    cp_w = 3850,                # specific heat of sea water at const pressure (in J/K/kg)
+    cp_a = 1005,                # specific heat of air at const pressure (in J/K/kg)
+    rho_w = 1027,               # density of sea water (in kg/m^3)
+    rho_a = 1.1,                # density of air (in kg/m^3)
+    n_w = 1.34,                 # refractive index of sea water
+    n_a = 1.0,                  # refractive index of air
+    C_s = 1.3e-3,               # turbulent exchange coefficient for sensible heat
+    C_l = 1.5e-3,               # turbulent exchange coefficient for latent heat
+    L_evap = 2.5e6,             # latent heat of evaporization (J/kg) Source: Klose p.151
+    sb_const = 5.67e-8,         # Stefan Boltzmann constant (in W/m^2/K^4)
+    gas_const = 461.51):        # gas constant of water vapor, J/K/kg):
 
-    # Define depth mesh
+    # Define vertical grid
     N_z = ngrid + 2
     z, stretch = make_mesh(dz,ngrid,z_f=z_f)
+    zidx_ref = np.where(z>-z_ref)[0][-1] # used only for STAB model
 
-    # Define time array
-    N_t = len(times)
-    dt = times[1:]-times[:-1]
-
-    # Atmospheric input data
-    s_a = sa_data
-    u = u_data
-
-    # Initialize temperature array and heat flux arrays
-    T = np.zeros((N_t, N_z))
-
-    # initial condition
-    T[0] = np.ones(N_z)*T_f
-
-    # boundary condition
-    T[:,-1] = np.ones(N_t)*T_f
-
-    # Eddy diffusivity
-    kappa = np.zeros((N_t,N_z))
-
-    for i in range(N_z):
-        kappa[:,i] = (k_mol - k_eddy * z[i]/z_f *(np.minimum(u,np.ones(N_t)*maxwind))**2)
-
-    # mixing coefficient
-    mix = np.zeros(N_z)
-    mix[1:-1] = mu * np.abs(z[2:]-z[1:-1]) / np.abs(z[1:-1]-z[-1])
-
-    # initialize heat fluxes
-    Qs, Ql, Rlw = [], [], []
-    R_sw = np.zeros((N_t,N_z))
-
-    # solar irradiance
-    solangle = 2*np.pi*times/86400 + np.pi
-    for i in range(N_z):
-        R_sw[:,i] = sw_data * np.exp(attenu/np.cos(snell(solangle))*z[i])
-
-    # stretched grid prerequisites
+    # Stretched grid derivatives
     dv1 = dndz(z, dz0=dz,eps=stretch)
     dv2 = dndz2(z, dz0=dz,eps=stretch)
+
+    # Define time stepping
+    N_t = len(time_data)
+    dt = time_data[1:]-time_data[:-1]
+
+    # Initialize temperature, heat flux, and diffusivity arrays
+    T      = np.zeros((N_t,N_z))    # Water temperature
+    diffu  = np.zeros((N_t,N_z))    # Eddy diffusivity
+    ddiffu = np.zeros((N_t,N_z))    # Spatial derivative of eddy diffusivity
+    R_sw   = np.zeros((N_t,N_z))    # Shortwave heat flux
+    Q_s, Q_l, R_lw = [], [], []     # Sensible, latent, and longwave heat fluxes
+
+    # Initial condition
+    if init is None:
+        T[0] = np.ones(N_z) * T_f
+    else:
+        T[0] = np.ones(N_z) * init
+
+    # Boundary condition
+    T[:,-1] = np.ones(N_t) * T_f
+
+    # Mixing coefficient
+    mix = np.zeros(N_z)
+    mix[1:-1] = mu / np.abs(z[1:-1]-z[-1]) * np.abs(z[2:]-z[1:-1])
+
+    # Shortwave heat flux
+    solar_angle = 2*np.pi*time_data/86400 + np.pi
+    for i in range(N_z):
+        R_sw[:,i] = swave_data * np.exp(alpha/np.cos(snell(solar_angle))*z[i])
+
+    # Generate wind speed array capped at wind_max and taken to the power of wind_exp
+    wind_factor = np.minimum(wind_data,wind_max)**wind_exp
+
+    # Eddy diffusivity
+    if diffu_type == 'BASE':
+        for i in range(N_z):
+            diffu[:,i] = k_mol + wind_factor * kappa
+
+    elif diffu_type == 'LIN':
+        for i in range(N_z):
+            diffu[:,i] = k_mol + wind_factor * kappa * np.abs(z[i]/z_f)
+            ddiffu[:,i] = - wind_factor * kappa / z_f
+
+    elif diffu_type == 'EXP':
+        for i in range(N_z):
+            diffu[:,i] = k_mol + wind_factor * kappa * (1-sigma*np.exp(z[i]/lambd))/(1-k_0*np.exp(-z_f/lambd))
+            ddiffu[:,i] = - wind_factor * kappa / lambd * sigma*np.exp(z[i]/lambd) /(1-k_0*np.exp(-z_f/lambd))
 
     # Time integration
     for n in range(1, N_t):
 
-        # surface fluxes
-        R_lw = sb_const * (opac*(Ta_data[n-1])**4 - (T[n-1,1])**4)
-        Q_s  = rho_a * c_p_a * C_s * max(0.5, u[n-1]) * (Ta_data[n-1] - T[n-1,1])
-        Q_l  = rho_a * L_evap * C_l * max(0.5, u[n-1]) * (s_a[n-1] - s_sat(T[n-1,1], rho_a, R_v))
+        # Compute surface fluxes
+        Rlw = sb_const * (opac*(airtemp_data[n-1])**4 - (T[n-1,1])**4)
+        Qs  = rho_a * cp_a * C_s * max(0.5, wind_data[n-1]) * (airtemp_data[n-1] - T[n-1,1])
+        Ql  = rho_a * L_evap * C_l * max(0.5, wind_data[n-1]) * (humid_data[n-1] - s_sat(T[n-1,1], rho_a, gas_const))
 
         # Total heat flux
         Q = R_sw[n-1]
-        #Q[0] = rad*R_sw[n-1,1] + surflux*(R_lw + Q_s + Q_l)
-        Q[0] += (R_lw + Q_s + Q_l)
+        Q[0] += (Rlw + Qs + Ql)
+
+        # STAB model
+        if diffu_type == 'STAB':
+            S = min( max((T[n-1,1]-T[n-1,zidx_ref]), 0) , 1)
+            diffu[n-1] = k_mol + wind_factor[n-1] * kappa * (1-S*sigma*np.exp(z/lambd))/(1-S*sigma*np.exp(-z_f/lambd))
+            ddiffu[n-1] = - wind_factor[n-1] * kappa * S*sigma/lambd * np.exp(z/lambd) /(1-S*sigma*np.exp(-z_f/lambd))
 
         # Euler integration
         T[n] = T[n-1] + dt[n-1] * (
-                kappa[n-1]*laplace_central(T[n-1],dv1,dv2) - k_eddy/z_f *(min(u[n-1],maxwind))**2*grad_central(T[n-1],dv1)
+                diffu[n-1]*laplace_central(T[n-1],dv1,dv2)
+                + ddiffu[n-1]*grad_central(T[n-1],dv1)
                 - mix*(T[n-1]-T_f)
-                + grad_backward(Q,dv1)/(rho_w*c_p)
-                )
+                + grad_backward(Q,dv1)/(rho_w*cp_w))
 
-    return [T[:,1:], z[1:], times]
+        # Write heat fluxes
+        if output == 'detailed':
+            R_lw.append(Rlw)
+            Q_s.append(Qs)
+            Q_l.append(Ql)
+
+    if output == 'temp':
+        return T[:,1:]
+    elif output == 'basic':
+        return [T[:,1:], z[1:], time_data]
+    elif output == 'detailed':
+        return [T[:,1:], z[1:], time_data, [Q_s, Q_l, R_lw, R_sw[:,1]]]
