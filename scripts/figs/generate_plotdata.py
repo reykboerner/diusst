9d58549785724cd0f88ddbac0ef3e43f0f5f65d7
reykboerner <reyk.boerner@reading.ac.uk>
2023-03-07 23:50:52 +0000

    new figs with added ZB05 results

--- scripts/figs/generate_plotdata.py
+++ scripts/figs/generate_plotdata.py
@@ -11,7 +11,9 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import sys
 sys.path.append('../../src/')
+from atmosdata import AtmosData
 from diusst import Diusst
+from zengbeljaars import ZengBeljaars
 from slab import Slab
 
 # Load forcing data set
@@ -24,24 +26,36 @@ T_f = float(data.T_f)
 # Parameter values - diuSST
 kappa, mu, alpha = 0.00013400773159919014, 0.002848626826335415, 3.518014095655488
 
+# Parameter values - ZB05
+nu, z_wind, z_rough, takaya10, skin_iter = 0.3, 20, None, True, 3
+
 # Parameter values - slab
 d, Q_sink, xi_1, xi_2 = 1.2010661501536875, 92.6740215866626, 0.00011854796494168126, 3.121690816078168e-12
 
 # Instantiate models
 model_diusst = Diusst(T_f=T_f, kappa=kappa, mu=mu, alpha=alpha, sigma=0.8, lambd=3, diffu_profile='LIN', CFL=0.95, z_f=10, dz0=0.1, ngrid=40)
+model_zb05 = ZengBeljaars(T_f=T_f, z_wind=z_wind, nu=nu, z_rough=z_rough, takaya10=takaya10)
 model_slab = Slab(d=d, Q_sink=Q_sink, xi_1=xi_1, xi_2=xi_2, T_f=T_f)
 model_const = Slab(d=1e5, Q_sink=0, xi_1=1e-3, xi_2=0, T_f=T_f)
 
 # Interpolate data in time
 data_intp, dtlist, idx = model_diusst.interpolate(data)
+atmosdata = AtmosData(data_intp)
 
 # Run simulations
 print('... Run diuSST model')
 T_diusst, t_diusst, z_diusst, Q_diusst = model_diusst.simulate(data_intp, output='detailed')
 Qs_diusst, Ql_diusst, Rlw_diusst, Rsw_diusst = Q_diusst
+
+print('... Run ZB05 model')
+x_zb05, y_zb05, delta_zb05, Q_zb05 = model_zb05.simulate(atmosdata, [0,0,0.001], skin_iter=skin_iter, output="all")
+Rlw_zb05, Qs_zb05, Ql_zb05 = Q_zb05
+T_zb05 = x_zb05 + y_zb05
+
 print('... Run slab model')
 T_slab, Q_slab = model_slab.simulate(data_intp, output='detailed')
 Qs_slab, Ql_slab, Rlw_slab = Q_slab
+
 print('... Run constant SST simulation')
 T_const, Q_const = model_const.simulate(data_intp, output='detailed')
 Qs_const, Ql_const, Rlw_const = Q_const
@@ -52,6 +66,12 @@ t_diusst = t_diusst[idx].copy()
 Qs_diusst = Qs_diusst[idx].copy()
 Ql_diusst = Ql_diusst[idx].copy()
 Rlw_diusst = Rlw_diusst[idx].copy()
+
+T_zb05 = T_zb05[idx].copy()
+Qs_zb05 = Qs_zb05[idx].copy()
+Ql_zb05 = Ql_zb05[idx].copy()
+Rlw_zb05 = Rlw_zb05[idx].copy()
+delta_zb05 = delta_zb05[idx].copy()
 
 T_slab = T_slab[idx].copy()
 Qs_slab = Qs_slab[idx].copy()
@@ -64,12 +84,15 @@ Ql_const = Ql_const[idx].copy()
 Rlw_const = Rlw_const[idx].copy()
 
 simu_diusst = T_diusst, t_diusst, z_diusst, [Qs_diusst, Ql_diusst, Rlw_diusst]
+simu_zb05 = T_zb05, delta_zb05, [Qs_zb05, Ql_zb05, Rlw_zb05]
 simu_slab = T_slab, [Qs_slab, Ql_slab, Rlw_slab]
 simu_const = T_const, [Qs_const, Ql_const, Rlw_const]
 
 # Store simulation data
 np.save('../../output_files/simu_diusst.npy', simu_diusst, allow_pickle=True)
 print('... saved simulation output as {}'.format('../../output_files/simu_diusst.npy'))
+np.save('../../output_files/simu_zb05.npy', simu_zb05, allow_pickle=True)
+print('... saved simulation output as {}'.format('../../output_files/simu_zb05.npy'))
 np.save('../../output_files/simu_slab.npy', simu_slab, allow_pickle=True)
 print('... saved simulation output as {}'.format('../../output_files/simu_slab.npy'))
 np.save('../../output_files/simu_const.npy', simu_const, allow_pickle=True)
